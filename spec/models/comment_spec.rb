ec2ec93c91078eabc374a2ae12e0f0f1d4e4349a
Tom Paganel <paganel.tom@gmail.com>
2022-04-08 16:04:19 +0200

    test to DB and not memory, still have issue with rspec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe Comment, type: :model do
       expect(comment.errors.messages).to include(:content)
       expect(comment.valid?).to be false
     end
+
+    it 'should accept valid concent' do
+      comment = build(:comment, post: create(:post))
+      comment.validate
+      expect(comment.valid?).to be true
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   context 'comment association' do
     it 'should increment comments_count' do
-      post = build(:post)
-      post.comments << build(:comment)
+      post = Post.new(name: 'salut')
+      post.comments << Comment.new(content: 'AZERTYUOIPGLGR')
       post.save
       expect(Comment.count).to eq(1)
       expect(post.comments_count).to eq(1)
     end
-  end
-  it 'should persist a post' do
-    create(:post)
-    expect(Post.count).to eq(1)
+    it 'should persist a post' do
+      Post.create(name: 'test')
+      expect(Post.count).to eq(1)
+    end
   end
 end
